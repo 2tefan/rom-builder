d2740a381471632acdaedd204d3dee879cf04439
2tefan <2tefan@tuta.io>
2022-08-18 21:26:25 +0200

    Prepare CI/CD

--- build.sh
+++ build.sh
@@ -1,10 +1,26 @@
 #!/bin/env bash
 
 VARIABLES_BEFORE=$(compgen -v)
+for arg in "$@"; do
+    case "${arg}" in
+    --push | -p)
+        PUSH=YES
+        ;;
+    --linux-distro=*)
+        LINUX_DISTRO="${arg#*=}"
+        ;;
+    --linux-distro-release=*)
+        LINUX_DISTRO_RELEASE="${arg#*=}"
+        ;;
+    esac
+done
+
 : "${IMAGE_NAME:=2tefan/android-rom-builder}"
 : "${LINUX_DISTRO:=ubuntu}"
 : "${LINUX_DISTRO_RELEASE:=20.04}"
 : "${BUILDER_VERSION:=$(git describe --dirty)}"
+: "${IMAGE:="${IMAGE_NAME}:${BUILDER_VERSION}-${LINUX_DISTRO}-${LINUX_DISTRO_RELEASE}"}"
+: "${PUSH:=NO}"
 
 : "${SCRIPT:=$(realpath "$(basename "$0")")}"
 : "${REPO_ROOT:=$(dirname "${SCRIPT}")}"
@@ -17,4 +33,10 @@ for VARIABLE in ${NEW_VARIABLES[@]}; do
     echo "${VARIABLE}=${!VARIABLE}"
 done
 
-docker build "${REPO_ROOT}" -t "${IMAGE_NAME}:${BUILDER_VERSION}-${LINUX_DISTRO}-${LINUX_DISTRO_RELEASE}"
+docker build "${REPO_ROOT}" -t "${IMAGE}"
+
+
+if [ "${PUSH}" = "YES" ]; then
+    docker login --username "${DOCKER_USERNAME}" --password "${DOCKER_PASSWORD}"
+    docker push "${IMAGE}"
+fi
